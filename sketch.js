3b6684587198e8478f4c21db030758992564a85a
Alan da Matta <alandamatta@live.com>
2021-05-05 07:36:50 -0300

    bar count workaround (not definitive)

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 	let myCanvas = createCanvas(window.innerWidth, window.innerHeight - 200);
 	myCanvas.parent('canvasDiv');
 	let quantity =( window.innerWidth / 55) - 13;
-	for (let i = 0; i < quantity; i++) {
+	for (let i = 0; i < quantity || i < 6; i++) {
 		array.push(Math.round(random(innerHeight - 400)));
 	}
 	noLoop();
